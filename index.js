307b825a5e0f12968f943e2aa398a084b2d03c91
samuel-barkley <samuelbarkley2001@gmail.com>
2022-06-04 19:27:54 +0200

    Removing goals and if to switch

--- index.js
+++ index.js
@@ -19,21 +19,25 @@ client.on("messageCreate", (message) => {
   if (message.content.startsWith(prefix)) {
     var command = message.content.substring(prefix.length).split(" ");
 
-    if (command[0] == "ping") {
-      message.channel.send("pong");
-    }
-    if (command[0] == "add") {
-      if (command.length == 3) {
-        if (isNaN(Number(command[2]))) {
-          message.channel.send(command[2] + ", is not a number.")
+    switch (command[0]) {
+      case "ping":
+        message.channel.send("pong");
+        break;
+      case "add":
+        if (command.length == 3) {
+          if (isNaN(Number(command[2]))) {
+            message.channel.send(command[2] + ", is not a number.")
+          } else {
+            goals.push({ name: command[1] })
+            Save(goals);
+            message.channel.send(command[2] + ", added to progress meter.")
+          }
         } else {
-          goals.push({ name: command[1] })
-          Save(goals);
-          message.channel.send(command[2] + ", added to progress meter.")
+          message.channel.send(`Add format is incorrect. Format is \`${prefix}add <goal> <amount>\``)
         }
-      } else {
-        message.channel.send(`Add format is incorrect. Format is \`${prefix}add <goal> <amount>\``)
-      }
+        break;
+      default:
+        break;
     }
   }
 });
